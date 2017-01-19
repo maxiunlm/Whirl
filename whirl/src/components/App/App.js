3644f1539560d31d1adc85200a40150ad6424494
Maximiliano Gauna <maximilianog@lagash.com>
2017-01-19 17:31:12 -0300

    Pruebas de interaccion entre componentes

--- whirl/src/components/App/App.js
+++ whirl/src/components/App/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from '../../logo.svg';
+/* global actions */
+
+import React, { Component }
+from 'react';
 import './App.css';
+import UserSpaceShip from '../UserSpaceShip/UserSpaceShip.js';
+import LeftDirection from '../Buttons/LeftDirection.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        this.width = screen.width;
+        
+        window.actions = {};
+        window.actions.moveUserSpaceShipToLeft = () => {};        
+        //UserSpaceShip.contextTypes = { actions: this.actions };
+    }
+
+    render() {
+        return (
+                <div>
+                    <UserSpaceShip gameHeight={this.height} gameWidth={this.width} />
+                    <LeftDirection action={() => window.actions.moveUserSpaceShipToLeft() }/>
+                            <button onClick={() => window.actions.moveUserSpaceShipToLeft()}>&lt; 2</button>
+                </div>
+                );
+    }
 }
 
 export default App;
--- whirl/src/index.js
+++ whirl/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import UserSpaceShip from './components/UserSpaceShip/UserSpaceShip.js';
+import App from './components/App/App.js';
 import './index.css';
 
-ReactDOM.render(
-  <UserSpaceShip />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
